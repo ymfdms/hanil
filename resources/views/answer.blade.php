731ab180852ed7dbdc69120ef9141c5857d9570c
ymfdms <ymfdms@gmail.com>
2020-09-30 22:18:08 +0900

    20200930

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -43,7 +43,6 @@
                     　</div>
                     </div>
                 </div>
-                
             </header>
             <main>
                 <div id="maru">○</div>
